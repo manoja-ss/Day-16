7927e62f774ee7fb93dbb3f87175ff39cb95b209
manoja-ss <manojass.in@gmail.com>
2025-12-27 00:26:57 +0530

    Update calculator logic and main class

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,24 @@
 class Calculator {
-
+	
+	int add(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+	
+	int sub(int a, int b) {
+		int result = a - b;
+		return result;
+	}
+	
+	int multiply(int a, int b) {
+		int result = a * b;
+		return result;
+	}
+	
+	double div(int a, int b) {
+		double a1 = a;
+		double b1 = b;
+		double result = a1 / b1;
+		return result;
+	}
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,35 @@
-
-public class Main {
+import java.util.Scanner;
+class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		Calculator c1 = new Calculator();
+		System.out.println("Enter number for addition:");
+		int a1 = scan.nextInt();
+		int b1 = scan.nextInt();
+		
+		int addResult = c1.add(a1, b1);
+		System.out.println("Addition Result is " + addResult);
+		
+		System.out.println("Enter number for Subtraction");
+		int a2 = scan.nextInt();
+		int b2 = scan.nextInt();
+		int subResult = c1.sub(a2, b2);
+		System.out.println("Subtraction result is " + subResult);
+		
+		System.out.println("Enter number for Multiplication: ");
+		int a3 = scan.nextInt();
+		int b3 = scan.nextInt();
+		int multiplyResult = c1.multiply(a3, b3);
+		System.out.println("Multiplication result is " + multiplyResult);
+		
+		System.out.println("Enter number for division: ");
+		int a4 = scan.nextInt();
+		int b4 = scan.nextInt();
+		double divResult = c1.div(a4, b4);
+		System.out.println(divResult);
+		
+		scan.close();
 	}
 
 }
